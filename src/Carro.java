352372bdb6367f6e0e85115bc2d6420060a8ef38
Douglas_Michelini <douglasmsrodrigues@hotmail.com>
2026-03-09 10:59:08 -0300

    Setup da tarefa

---
+++ src/Carro.java
@@ -0,0 +1,7 @@
+public class Carro {    
+    String marca = "Fusca"; // Propriedade que define a marca do carro
+    int ano = 1981; // Propriedade que define o ano do carro
+    public static void main(String args[]) {
+        System.out.println("Vrum vrum");
+    }
+}
